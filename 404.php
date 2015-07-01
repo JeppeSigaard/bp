ce34e747960e9d9b1dd5a66508c77aa1b84e6cac
JeppeSigaard <jeppe@smatmonkey.dk>
2015-07-01 16:39:57 +0200

    rettet 404.php

--- 404.php
+++ 404.php
@@ -1,7 +1,5 @@
 <?php /* 404 */ get_header(); ?>
-
-<p></p>
-<p style="text-align:center;">Siden blev ikke fundet :-(</p>
-<p></p>
-
+<div class="box-full">
+    <p style="text-align:center;">Siden blev ikke fundet :-(</p>
+</div>
 <?php get_footer(); ?>
